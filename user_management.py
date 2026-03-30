ff914dcef06a6c9cc78aa0fce3ce70dd4c13c793
JackMa222 <128107480+JackMa222@users.noreply.github.com>
2026-03-30 14:59:04 +1100

    Ensure similarity for login timing (PER REQUEST)
    This was requested as part of the task to make sure that the timing is the same, regardless of the efficencies.

--- user_management.py
+++ user_management.py
@@ -31,26 +31,29 @@ def retrieveUserId(username):
 def retrieveUsers(username, password):
     con = sql.connect("database_files/database.db")
     cur = con.cursor()
-    cur.execute(f"SELECT * FROM users WHERE username = ?", (username, ))
-    if cur.fetchone() == None:
-        con.close()
-        return False
+    cur.execute(f"SELECT password FROM users WHERE username = ?", (username, ))
+    row = cur.fetchone()
+    con.close()
+    
+    dummy_hash = "scrypt:32768:8:1$ee2iRzbDj6WUq9DP$b4c8f9a3a366ea0f5b954131c62e9fb5d100ca9128ba05b178f4d0581401380c0f122f3cd4484ca76319c67a1dc262640d4e70ce88819652e9429632182b7354"
+    
+    if row:
+        hashed_password = row[0]
+        user_exists = True
     else:
-        cur.execute(f"SELECT password FROM users WHERE username = ?", (username, ))
-        row = cur.fetchone()
-        con.close()
-        # Plain text log of visitor count as requested by Unsecure PWA management
-        with open("visitor_log.txt", "r") as file:
-            number = int(file.read().strip())
-            number += 1
-        with open("visitor_log.txt", "w") as file:
-            file.write(str(number))
-        # Simulate response time of heavy app for testing purposes
-        time.sleep(random.randint(80, 90) / 1000)
-        if row:
-            hashed_password = row[0]
-            return check_password_hash(hashed_password, password)
-        return False
+        hashed_password = dummy_hash
+        user_exists = False
+    
+    password_correct = check_password_hash(hashed_password, password)
+    
+    # Plain text log of visitor count as requested by Unsecure PWA management
+    with open("visitor_log.txt", "r") as file:
+        number = int(file.read().strip())
+        number += 1
+    with open("visitor_log.txt", "w") as file:
+        file.write(str(number))
+
+    return user_exists and password_correct
 
 
 def insertFeedback(feedback):
